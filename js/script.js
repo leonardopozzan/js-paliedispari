4966da95b78bd1328f07ec1d1fb0ed30052f59e2
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-14 20:39:18 +0200

    corretto

--- js/script.js
+++ js/script.js
@@ -4,6 +4,23 @@
 
 const bottone = document.querySelector('#btn');
 
+function palindromo(){
+    //catturo l'input dell'utente e il div dove stampo il risultato
+    const word = document.querySelector('#word').value.toLowerCase().trim();
+    const result = document.querySelector('#result');
+    //svuoto il div risultato
+    result.innerHTML = '';
+
+    const wordReversed = word.split('').reverse().join('');
+
+    console.log(word, wordReversed)
+    if( wordReversed == word){
+        result.innerHTML = `${word} è un parola palindroma`
+    }else{
+        result.innerHTML = `${word} non è un parola palindroma`
+    }
+}
+
 bottone.addEventListener('click',palindromo);
 
 // Pari e Dispari
@@ -14,5 +31,31 @@ bottone.addEventListener('click',palindromo);
 // Dichiariamo chi ha vinto.
 
 const bottone2 = document.querySelector('#btn-2');
+
+function play(){
+    //catturo gli input dell'utente e il div dove stampo il risultato
+    const oddEven = document.querySelector('#odd-even').value.toLowerCase().trim();
+    const userNumber = parseInt(document.querySelector('#user-number').value);
+    const result = document.querySelector('#result-2');
+    //svuoto il div risultato
+    result.innerHTML = '';
+    //controllo che l'utente abbia inserito i dati corretti
+    if (oddEven != 'pari' && oddEven != 'dispari' || !(0 <= userNumber <= 5)){
+        result.innerHTML = 'Inserisci solo pari o dispari e scegli un numero compreso tra 0 e 5'
+        return;
+    }
+
+    //creo il numero random del pc e lo sommo con quello dell'utente
+    const pcNumber = Math.floor(Math.random() * 5) + 1
+    const somma = userNumber + pcNumber;
+
+    //stampo il messaggio di vittoria o sconfitta nell'HTML
+    result.innerHTML = `Utente ha scelto: ${oddEven}  Utente: ${userNumber} PC: ${pcNumber} Somma: ${somma} `
+    if (oddEven == 'pari'&& somma%2 == 0 || oddEven == 'dispari' && somma%2 != 0){
+        result.innerHTML += "L'utente ha Vinto";
+    }else{
+        result.innerHTML += "Il PC ha Vinto";
+    }
+}
 
 bottone2.addEventListener('click',play);
